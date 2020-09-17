920a37efb09af32bc5f1855a09437074ac0b4357
unknown <ahmadalialdahesh@hotmail.com>
2020-09-17 17:42:51 +1000

    finished 1st quadrant of level generator

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -36,6 +36,7 @@ public class LevelGenerator : MonoBehaviour
     {
         //Instantiate(outsideWall, transform.position = new Vector3(-1f, -1f, -1f), Quaternion.Euler(0f, 0f, 90f));
 
+        //TOP LEFT
         for(int row = 0; row < levelMap.GetLength(0); row++)
         {
             for(int col = 0; col < levelMap.GetLength(1); col++)
@@ -47,14 +48,318 @@ public class LevelGenerator : MonoBehaviour
                     switch(levelMap[row, col])
                     {
                         case 1:
-                            Debug.Log("CASE IS 1");
-                            Instantiate(outsideCorner, transform.position = new Vector3((col - 13), (row + 14), 0), transform.rotation);
+                            Instantiate(outsideCorner, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
                             break;
                         case 2:
-                            Instantiate(outsideWall, transform.position = new Vector3((col - 13), (row + 14), 0), Quaternion.Euler(0f, 0f, 90f));
+                            Instantiate(outsideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, 90f));
                             break;
                         case 7:
-                            Instantiate(tJunction, transform.position = new Vector3((col - 13), (row + 14), 0), transform.rotation);
+                            Instantiate(tJunction, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                    }
+                }
+                else if(row == 1)
+                {
+                    switch(levelMap[row, col])
+                    {
+                        case 2:
+                            Instantiate(outsideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 5:
+                            Instantiate(standardPellet, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 4:
+                            Instantiate(insideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                    }
+                }
+                else if(row == 2)
+                {
+                    switch(levelMap[row, col])
+                    {
+                        case 2:
+                            Instantiate(outsideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 5:
+                            Instantiate(standardPellet, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 3:
+                            if(col == 5 || col == 11)
+                                Instantiate(insideCorner, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, -90f));
+                            else
+                                Instantiate(insideCorner, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 4:
+                            if(col == 13)
+                                Instantiate(insideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            else
+                                Instantiate(insideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, -90f));
+                            break;
+                    }
+                }
+                else if(row == 3)
+                {
+                    switch(levelMap[row, col])
+                    {
+                        case 2:
+                            Instantiate(outsideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 6:
+                            Instantiate(powerPellet, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 4:
+                            if(col == 5 || col == 11)
+                                Instantiate(insideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, 180f));
+                            else
+                                Instantiate(insideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 0:
+                            break;
+                        case 5:
+                            Instantiate(standardPellet, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                    }
+                }
+                else if(row == 4)
+                {
+                    switch(levelMap[row, col])
+                    {
+                        case 2:
+                            Instantiate(outsideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 5:
+                            Instantiate(standardPellet, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 3:
+                            if(col == 5 || col == 11)
+                                Instantiate(insideCorner, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, 180f));
+                            else
+                                Instantiate(insideCorner, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, 90f));
+                            break;
+                        case 4:
+                            Instantiate(insideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, 90f));
+                            break;
+                    }
+                }
+                else if(row == 5)
+                {
+                    switch(levelMap[row, col])
+                    {
+                        case 2:
+                            Instantiate(outsideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 5:
+                            Instantiate(standardPellet, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                    }
+                }
+                else if(row == 6)
+                {
+                    switch(levelMap[row, col])
+                    {
+                        case 2:
+                            Instantiate(outsideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 5:
+                            Instantiate(standardPellet, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 3:
+                            if(col == 5 || col == 8)
+                                Instantiate(insideCorner, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, -90f));
+                            else
+                                Instantiate(insideCorner, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 4:
+                            Instantiate(insideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, -90f));
+                            break;
+                    }
+                }
+                else if(row == 7)
+                {
+                    switch(levelMap[row, col])
+                    {
+                        case 2:
+                            Instantiate(outsideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 5:
+                            Instantiate(standardPellet, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 3:
+                            if(col == 5)
+                                Instantiate(insideCorner, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, 180f));
+                            else if(col == 2 || col == 10)
+                                Instantiate(insideCorner, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, 90f));
+                            else
+                                Instantiate(insideCorner, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 4:
+                            if(col == 7)
+                                Instantiate(insideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            else if(col == 8)
+                                Instantiate(insideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, 180f));
+                            else
+                                Instantiate(insideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, 90f));
+                            break;
+                    }
+                }
+                else if(row == 8)
+                {
+                    switch(levelMap[row, col])
+                    {
+                        case 2:
+                            Instantiate(outsideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 5:
+                            Instantiate(standardPellet, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 4:
+                            if(col == 8)
+                                Instantiate(insideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, 180f));
+                            else
+                                Instantiate(insideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                    }
+                }
+                else if(row == 9)
+                {
+                    switch(levelMap[row, col])
+                    {
+                        case 1:
+                            if(col == 5)
+                                Instantiate(outsideCorner, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, -90f));
+                            else
+                                Instantiate(outsideCorner, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, 90f));
+                            break;                        
+                        case 2:
+                            Instantiate(outsideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, 90f));
+                            break;
+                        case 5:
+                            Instantiate(standardPellet, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 4:
+                            if(col == 7 || col == 13)
+                                Instantiate(insideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            else
+                                Instantiate(insideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, -90f));
+                            break;
+                        case 3:
+                            if(col == 8)
+                                Instantiate(insideCorner, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            else
+                                Instantiate(insideCorner, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, -90f));
+                            break;
+                        case 0:
+                            break;
+                    }
+                }
+                else if(row == 10)
+                {
+                    switch(levelMap[row, col])
+                    {
+                        case 0:
+                            break;
+                        case 2:
+                            Instantiate(outsideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 5:
+                            Instantiate(standardPellet, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 4:
+                            if(col == 7 || col == 13)
+                                Instantiate(insideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            else
+                                Instantiate(insideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, 90f));
+                            break;
+                        case 3:
+                            if(col == 11)
+                                Instantiate(insideCorner, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, 180f));
+                            else
+                                Instantiate(insideCorner, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, 90f));
+                            break;
+                    }
+                }
+                else if(row == 11)
+                {
+                    switch(levelMap[row, col])
+                    {
+                        case 0:
+                            break;
+                        case 2:
+                            Instantiate(outsideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 5:
+                            Instantiate(standardPellet, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 4:
+                            if(col == 7)
+                                Instantiate(insideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            else
+                                Instantiate(insideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, 180f));
+                            break;
+                    }
+                }
+                else if(row == 12)
+                {
+                    switch(levelMap[row, col])
+                    {
+                        case 0:
+                            break;
+                        case 2:
+                            Instantiate(outsideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 5:
+                            Instantiate(standardPellet, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 4:
+                            if(col == 7)
+                                Instantiate(insideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            else if(col == 8)
+                                Instantiate(insideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, 180f));
+                            else
+                                Instantiate(insideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, -90f));
+                            break;
+                        case 3:
+                            Instantiate(insideCorner, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                    }
+                }
+                else if(row == 13)
+                {
+                    switch(levelMap[row, col])
+                    {
+                        case 2:
+                            Instantiate(outsideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, 90f));
+                            break;
+                        case 1:
+                            Instantiate(outsideCorner, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, -180f));
+                            break;
+                        case 5:
+                            Instantiate(standardPellet, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 3:
+                            if(col == 7)
+                                Instantiate(insideCorner, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, 90f));
+                            if(col == 8)
+                                Instantiate(insideCorner, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), Quaternion.Euler(0f, 0f, 180f));
+                            break;
+                        case 0:
+                            break;
+                        case 4:
+                            Instantiate(insideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                    }
+                }
+                else if(row == 14)
+                {
+                    switch(levelMap[row, col])
+                    {
+                        case 0:
+                            break;
+                        case 5:
+                            Instantiate(standardPellet, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
+                            break;
+                        case 4:
+                            Instantiate(insideWall, transform.position = new Vector3((col - 13), (row*-1 + 14), 0), transform.rotation);
                             break;
                     }
                 }
