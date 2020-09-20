4f1753e2454c77031cf0430c3e7e5cfaca270c25
unknown <ahmadalialdahesh@hotmail.com>
2020-09-20 13:55:02 +1000

    added final audio and finishing touches

---
+++ Assets/Scripts/Audio.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    [SerializeField] private AudioSource foxMoving;
+    private float globalCounter = 0f;
+    private bool soundOn = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        foxMoving.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        globalCounter += Time.deltaTime;
+        
+        if(globalCounter > 5)
+        {        
+            if(soundOn == false)
+            {
+                soundOn = true;
+                foxMoving.Play();
+            }
+        }
+    }
+}
